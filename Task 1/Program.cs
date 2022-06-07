2df93d3d73c30c2f75d8632f47b47c36ff9638af
«Mikhail» <kopfgolova@gmail.com>
2022-06-07 15:50:45 +0300

    add Task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -3,4 +3,18 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
-Console.WriteLine("Hello, World!");
+int SumFromMToN(int numberM, int numberN, int sum)
+{
+    if (numberM == numberN) return sum + numberM;
+    sum += numberM;
+    numberM++;
+    return SumFromMToN(numberM, numberN, sum);
+}
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+int result = SumFromMToN(m, n, sum);
+Console.WriteLine("Сумма элементов от M до N равна: ");
+Console.Write(result);
